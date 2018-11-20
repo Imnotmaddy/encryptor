388d6166942fd97f27ed3fcdb634b5774384541d
Mike <mr.mouse072@gmail.com>
2018-11-20 12:47:58 +0300

    FileReader and FileWrite are completed

--- src/alifanov.m/crypto.aes128/fileManagers/FileReader.java
+++ src/alifanov.m/crypto.aes128/fileManagers/FileReader.java
@@ -1,4 +1,33 @@
 package alifanov.m.crypto.aes128.fileManagers;
 
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 public class FileReader {
+    private String filePath;
+    private long fileSize;
+    private static int blockSize = 16;
+
+    public FileReader(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public byte[] readBlock(int offset) throws IOException {
+        byte[] data = new byte[blockSize];
+        RandomAccessFile file = new RandomAccessFile(filePath, "r");
+        long currentPointer = file.getFilePointer();
+        file.seek(currentPointer + offset);
+        file.read(data, 0, blockSize);
+        file.close();
+        return data;
+    }
+
+    public long getFileSize() throws IOException {
+        RandomAccessFile file = new RandomAccessFile(filePath, "r");
+        this.fileSize = file.length();
+        return fileSize;
+    }
+
+
 }
--- src/alifanov.m/crypto.aes128/fileManagers/FileWriter.java
+++ src/alifanov.m/crypto.aes128/fileManagers/FileWriter.java
@@ -1,4 +1,36 @@
 package alifanov.m.crypto.aes128.fileManagers;
 
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 public class FileWriter {
+    private String filePath;
+    private int[] block = new int[blockSize];
+    private static int matrixSize = 4;
+    private static int blockSize = 16;
+
+    public FileWriter(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public void writeBlock(int offset, int[][] data) throws IOException {
+        RandomAccessFile file = new RandomAccessFile(filePath, "rw");
+        matrixToArray(data);
+        file.seek(file.getFilePointer() + offset);
+        for (int i = 0; i < blockSize; i++) {
+            file.write(block[i]);
+        }
+        file.close();
+    }
+
+    private void matrixToArray(int[][] data) {
+        int k = 0;
+        for (int i = 0; i < matrixSize; i++) {
+            for (int j = 0; j < matrixSize; j++, k++) {
+                block[k] = data[j][i];
+            }
+        }
+    }
+
+
 }
